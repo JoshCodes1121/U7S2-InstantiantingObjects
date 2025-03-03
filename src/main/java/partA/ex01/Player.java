d9aac1f1109405a7642d9322f2e60619b3648a64
JoshuaAdenusi-Prince <joshua.adenusi-prince@cdpipelinedevshop.com>
2025-03-03 18:57:42 -0500

    Update Player.java

--- src/main/java/partA/ex01/Player.java
+++ src/main/java/partA/ex01/Player.java
@@ -1,46 +1,37 @@
 package partA.ex01;
 
+import static java.lang.Integer.parseInt;
+
 public class Player {
+    public String variable, height, name, yearsInLeague, playerNum;
+
     public static String player() {
-        String response = "\n"+"Player 1: \n" +
-                "variable: Stephen Curry\n" +
-                "Name: Stephen Curry\n" +
-                "Height: 6'2\n" +
-                "Years in the League: 13 years\n" +
-                "\n" +
-                "Player 2:\n" +
-                "variable: Michael Jordan \n" +
-                "Name: Michael Jordan\n" +
-                "Height: 6'6\n" +
-                "Years in the League: 15 years";
-
-
+        String response = "";
         /**
          * Your code goes here
          */
+        Player playerOne = new Player();
+        playerOne.playerNum = "Player 1:";
+        playerOne.variable = "variable: Stephen Curry";
+        playerOne.name = "Name: Stephen Curry";
+        playerOne.height = "Height: 6'2";
+        playerOne.yearsInLeague = "Years in the League: 13 years";
 
+        Player playerTwo = new Player();
+        playerTwo.playerNum = "Player 2:";
+        playerTwo.variable = "variable: Michael Jordan";
+        playerTwo.name = "Name: Michael Jordan";
+        playerTwo.height = "Height: 6'6";
+        playerTwo.yearsInLeague = "Years in the League: 15 years";
 
-        String variable, height, name, years, player;
-
-        player = "Player 1:";
-        variable = "variable: Stephen Curry";
-        name = "Name: Stephen Curry";
-        height = "Height: 6'2";
-        years = "Years in the league: 13 years";
-
-        System.out.println(player + "\n"+ variable + "\n"+ name  + "\n" + height+ "\n"+ years);
-        System.out.println("\n");
-
-        player = "Player 2:";
-        variable = "variable: Michael Jordan";
-        name = "Name: Michael Jordan";
-        height = "Height: 6'6";
-        years = "Years in the league: 15 years";
-
-        System.out.println(player + "\n"+ variable + "\n"+ name  + "\n" + height+ "\n"+ years);
-
+         response = playerOne.playerNum + "\n" + playerOne.variable +"\n" +
+                playerOne.name +"\n" + playerOne.height +"\n" +
+                playerOne.yearsInLeague +"\n" + "\n"+ playerTwo.playerNum +"\n" +
+                playerTwo.variable+"\n"+ playerTwo.name + "\n" + playerTwo.height +"\n" +
+                playerTwo.yearsInLeague;
 
         return response;
+
     }
 
     public static void main(String[] args) {
